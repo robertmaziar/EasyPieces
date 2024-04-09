867f790b8d0f9650f0bc4ddd1d037eaa1e38e78a
Robert Maziar <robert.maziar@gmail.com>
2024-04-09 10:38:36 -0400

    Delete EasyPieces.Attributes directory

--- EasyPieces.Attributes/IEasyAccessControlService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EasyPieces.Attributes
-{
-    public interface IEasyAccessControlService
-    {
-        public bool HasEasyAccess(List<string> allowedAccessors, int? userId = null);
-        public bool HasEasyAccess(List<string> allowedAccessors, string? userId = null);
-    }
-}
